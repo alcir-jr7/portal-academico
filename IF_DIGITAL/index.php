a6f61d046faf2ea59a595901b8f47dc3580e60c1
MikaSantos01 <micaias08deus@gmail.com>
2025-07-05 11:06:06 -0300

    Add files via upload

---
+++ IF_DIGITAL/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>IF_DIGTIAL</title>
+</head>
+<body>
+    <h1><span>IF</SPan>D</h1>
+
+    <div class="container">
+
+        <div class="professor">
+            <a href=""><img src="images/professor.png" alt="Professor"></a>
+                <h2>PROFESSOR</h2>
+        </div>
+
+        <div class="aluno">
+             <a href=""><img src="images/aluno.jpg" alt="Aluno"></a>
+                <h2>ALUNO</h2>
+        </div>
+
+        <div class="adm">
+             <a href=""><img src="images/adm.jpg" alt="adm"></a>
+                <h2>ADM</h2>
+        </div>
+
+    </div>
+
+    <footer> 
+         <p>&copy; 2025 Sistema Acadêmico | IF_DIGITAL | Desenvolvido por Mikaias | Alcir | Marília | Lincon</p>
+    </footer>
+</body>
+</html>
