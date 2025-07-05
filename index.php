d4fc1408a757be216990249d1d1cf4335e760237
MikaSantos01 <micaias08deus@gmail.com>
2025-07-05 12:40:56 -0300

    Foi feito a estrutura e o cs do index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="IF_Digital/public/recursos/css/geral.css">
+    <title>IF_DIGTIAL</title>
+</head>
+<body>
+
+    <h1>BEM VINDO AO</h1>
+    <h2><span>I</SPan>Campus</h2>
+
+    <div class="container">
+
+        <div class="professor">
+            <a href=""><img src="IF_Digital/public/recursos/images/professor.png" alt="Professor"></a>
+                <h3>PROFESSOR</h3>
+        </div>
+
+        <div class="aluno">
+             <a href=""><img src="IF_Digital/public/recursos/images/aluno.jpg" alt="Aluno"></a>
+                <h3>ALUNO</h3>
+        </div>
+
+        <div class="adm">
+             <a href=""><img src="IF_Digital/public/recursos/images/adm.jpg" alt="adm"></a>
+                <h3>ADM</h3>
+        </div>
+
+    </div>
+
+    <footer> 
+         <p>&copy; 2025 Sistema Acadêmico | IF_DIGITAL | Desenvolvido por Mikaias | Alcir | Marília | Lincon</p>
+    </footer>
+</body>
+</html>
