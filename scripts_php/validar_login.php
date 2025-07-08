0e4cce4164cd637dcc65a22e68b4683b15971ecc
MikaSantos01 <micaias08deus@gmail.com>
2025-07-08 00:19:10 -0300

    Foi feito a conexão do banco de dados

---
+++ scripts_php/validar_login.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$pdo = new PDO("mysql:host=localhost;dbname=seu_banco", "root", "");
+
+// Pega dados do formulário
+$usuario = $_POST['usuario'] ?? '';
+$senha = $_POST['senha'] ?? '';
+
+// Validação simples
+if ($usuario === $adminUsuario && $senha === $adminSenha) {
+    $_SESSION['admin_logado'] = true;
+    header('Location: ../publico/painel_admin.php');
+    exit;
+} else {
+    echo "Acesso negado. <a href='../publico/login.php'>Tente novamente</a>";
+}
+?>
